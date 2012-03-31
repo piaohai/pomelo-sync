80ceaaaac764e4ed9f0ac54ad192a6e540c5ee63
Administrator <sailor@ubuntu.ubuntu-domain>
2012-03-31 14:52:31 +0800

    msic:init project

---
+++ test/datasyncTest.js
@@ -0,0 +1,34 @@
+try {
+  var dbsync = require('dbsync');
+} catch (err) {
+  var dbsync = require('../');
+}
+
+
+var options = {}
+
+var timeout = 1000;
+
+var server = dbsync.create(options);
+
+var User = function User(name){
+	this.name = name;
+};
+
+var user1 = new User('hello');
+var user2 = new User('world');
+
+server.register(user1,user1,function(err,resp){
+	console.log('app.js register user1 result error=' + JSON.stringify(err) + ' resp ' + JSON.stringify(resp));
+});
+
+server.register(user2,user2,function(err,resp){
+	console.log('app.js register user2  result error=' + err + ' resp ' + JSON.stringify(resp));
+});
+
+server.get(user1,function(err,resp){
+	console.log('app.js get  result error=' + err + ' resp ' + JSON.stringify(resp));
+});
+
+
+setInterval(function(){user1.name = 'hello change ';console.log('change user name');},1000* 60 *1);
